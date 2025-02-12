b2b1d30a5b726dd08bc0817aa3e0d928af4096d0
Mohemmed Mossa Abdul-riza <moab015@student.kristiania.no>
2025-02-12 16:33:19 +0100

    init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// @ts-ignore
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
 })
